710014450b28eab46ae4c55f2f34a7e4556dad5f
supersiem <vankeulensiem@gmail.com>
2025-02-15 18:40:21 +0100

    dark mode knop

---
+++ js/darkmode.js
@@ -0,0 +1,12 @@
+let darkmode = Boolean(localStorage.getItem("darkmode"))
+// warn: dit bestaat nog niet en doet nog niks
+function toggle_darkmode() {
+    darkmode = !darkmode
+    localStorage.setItem("darkmode", darkmode)
+}
+function update_darkmode() {
+    // TODO: dit maken
+
+    console.log(darkmode)
+}
+update_darkmode()
