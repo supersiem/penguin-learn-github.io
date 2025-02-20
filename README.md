5632e867eb44728819b4426a98e30705e159b276
supersiem <vankeulensiem@gmail.com>
2025-02-20 16:57:05 +0100

    gyeygey

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 <div align=center>
- <img src="https://github.com/studyGOgratis/penguin-learn/blob/main/IconKitchen-Output/web/apple-touch-icon.png?raw=true"/>
+ <img src="https://github.com/studyGOgratis/penguin-learn/blob/main/IconKitchen-Output/web/apple-touch-icon.png?raw=true"/> <br>
+<img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/penguin-learn/penguin-learn">
+<img alt="Static Badge" src="https://img.shields.io/badge/bevat-spaghetti_code-green">
+
+
 </div>
 
 ---
--- studygo.js
+++ studygo.js
@@ -1,11 +1,41 @@
-// WARN: dit doet het nog niet dus niet gebruiken
-async function upload_test() {
+// note: gebruik bij voorkeur https://corsproxy.io/ als token niet nodig is
+
+// WARN: LAAT DIT NIET AAN STAAN
+const negeer_token_vernieuwen_datum = false
+
+// WARN: DIT ZET ALLE STUDYGO FUNCTIES UIT DUS ZET NIET AAN
+const gebruik_studygo_api = true
+
+
+async function get_user_data() {
+    if(!gebruik_studygo_api) return
+    let myHeaders = new Headers();
+    myHeaders.append("x-auth-token", await get_token());
+
+    try {
+        const response = await fetch(
+            'https://cros.vankeulensiem.workers.dev/?url=' +
+            encodeURIComponent("https://api.wrts.nl/api/v3/get_user_data"),
+            {
+                method: "GET",
+                headers: myHeaders,
+                redirect: "follow"
+            }
+        );
+
+        let result = await response.json();
+
+        return result;
+    } catch (error) { throw error }
+}
+async function get_token() {
+    if(!gebruik_studygo_api) return
     let token_vernieuwen_datum = localStorage.getItem("token_vernieuwen_datum");
     if (token_vernieuwen_datum == null) {
         localStorage.setItem("token_vernieuwen_datum", 1);
     }
 
-    if (token_vernieuwen_datum <= Number(Date.now().toString().slice(0, 10))) {
+    if (token_vernieuwen_datum <= Number(Date.now().toString().slice(0, 10)) || negeer_token_vernieuwen_datum) {
         console.log("token is bijna verlopen");
 
         let myHeaders = new Headers();
@@ -17,7 +47,7 @@ async function upload_test() {
 
         try {
             const response = await fetch(
-                'https://corsproxy.io/' + 
+                'https://corsproxy.io/?url=' +
                 encodeURIComponent("https://api.wrts.nl/api/v3/auth/get_token?email=penguinlearn@googlegroups.com&password=hoi als je dit ziet ben je in de code van de website aan het kijken "),
                 {
                     method: "POST",
@@ -32,8 +62,17 @@ async function upload_test() {
             // Update localStorage
             localStorage.setItem("token", result.auth_token.toString());
             localStorage.setItem("token_vernieuwen_datum", Number(result.renew_from));
+            return result.auth_token.toString()
         } catch (error) {
             console.error("Error:", error);
+            return localStorage.getItem("token")
         }
-    }
+
+    } else { return localStorage.getItem("token") }
+}
+
+async function upload_test() {
+    console.log(await get_user_data())
+
+
 }
