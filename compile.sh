384f395423f2472f64f99a50a5446e210e1bc9c2
supersiem <vankeulensiem@gmail.com>
2025-02-28 13:33:05 +0100

    auto build please see the release notes

--- compile.sh
+++ compile.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo "remove test stuff"
 rm -r custom-framework-tests
-echo "build minified version" 
 uglifyjs custom_framework.js -c -o custom_framework.min.js
-echo "comit and push"
 git add .
 git commit -m "auto build please see the release notes"
 git push
