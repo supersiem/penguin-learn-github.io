4cd674a6dda3dccb359159ff4eafbe24acb4bd21
supersiem <vankeulensiem@gmail.com>
2025-02-28 12:44:06 +0100

    update

--- compile.sh
+++ compile.sh
@@ -3,3 +3,7 @@ echo "remove test stuff"
 rm -r custom-framework-tests
 echo "build minified version" 
 uglifyjs custom_framework.js -c -o custom_framework.min.js
+echo "comit and push"
+git add .
+git commit -m "update"
+git push
