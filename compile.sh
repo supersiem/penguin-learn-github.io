9e740efb79728ac9a4535741f1ee150c1aa1f7e0
supersiem <vankeulensiem@gmail.com>
2025-02-28 13:44:55 +0100

    auto build please see the release notes

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -r custom-framework-tests
+rm .DS_Store
 uglifyjs custom_framework.js -c -o custom_framework.min.js
 git add .
 git commit -m "auto build please see the release notes"
